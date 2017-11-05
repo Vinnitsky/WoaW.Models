0fe158412e22def7e102a3f738c287dedd2fc5e9
Vinnitsky <vinnitsky@msn.com>
2017-11-05 20:59:20 +0200

    added products components: list

---
+++ ClientApp/modules/products/components/product-list-item.vue
@@ -0,0 +1,16 @@
+<template >
+    <div id="productListItem">  
+     
+    </div>
+</template>
+
+<script>
+export default {
+  name: "productListItem",
+  data() {
+    return {
+        items:[]
+    };
+  }
+};
+</script>

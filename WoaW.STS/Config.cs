177fb062eb3e4b5451fe6305c70e70945e19ab09
Vladimir Vinnitsky <vinnitsky@msn.com>
2017-11-18 14:23:44 +0200

    Rebuild solution folder

---
+++ WoaW.STS/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WoaW.STS
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAPiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource("app1", "My API")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client{
+                    ClientId="client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets=
+                    {
+                        //new Secret("secret", Sha256())
+                    },
+                    AllowedScopes= { "*api1"}
+                }
+            };
+        }
+    }
+}

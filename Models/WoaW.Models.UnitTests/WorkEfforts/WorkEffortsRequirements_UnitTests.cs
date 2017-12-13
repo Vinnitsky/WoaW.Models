aeedd1e578024e354894b4fe4f7dec32097c2d43
Vladimir Vinnitsky <vinnitsky@msn.com>
2017-12-13 15:48:10 +0200

    update tests

--- Models/WoaW.Models.UnitTests/WorkEfforts/WorkEffortsRequirements_UnitTests.cs
+++ Models/WoaW.Models.UnitTests/WorkEfforts/WorkEffortsRequirements_UnitTests.cs
@@ -14,6 +14,7 @@ using WoaW.Models.Parties.Root;
 namespace WoaW.Models.UnitTests
 {
     [TestClass]
+    [TestCategory("WorkEfforts")]
     public class WorkEffortsRequirements_UnitTests
     {
         /// <summary>
